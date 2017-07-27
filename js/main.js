795d823c823bd9d56700828fc54dee4ca0697b90
AnnaMkrtumyan <mkrtumian94@mail.ru>
2017-07-27 23:41:35 +0400

    Adding slider plugin

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 $(function() {
-	 $('.slider-for').slick({
-	  autoplay: true,
-	  speed: 2000,
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  arrows: true,
-	  fade: true,
-	  asNavFor: '.slider-nav',
-	  focusOnSelect: false
+	 $('.slider-for').waSlider({
+	  'autoplay': true,
+	  'speed': 5000,
+	  'slidesToScroll': 1,
+	  'marginsBtwnSlides': 20,
+	  'waResponsive': true,
+	  'arrows': true,
+	  'dots': true,
+	  'swipe': true,
+	  'animation':'cssEase'
 	});
-	$('.slider-nav').slick({
+	/*$('.slider-nav').slick({
 	  slidesToShow: 3,
 	  slidesToScroll: 1,
 	  asNavFor: '.slider-for',
@@ -18,7 +19,7 @@ $(function() {
 	  centerMode: true,
 	  focusOnSelect: false,
 	  variableWidth: true
-	});
+	});*/
 	/*$('.currency').slick({
 	  slidesToShow: 1,
 	  slidesToScroll: 1,
