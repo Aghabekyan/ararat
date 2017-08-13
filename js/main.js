10e562f6460b9fd7309db378101c00a4a2e840fb
AnnaMkrtumyan <annmkrtumyan@gmail.com>
2017-08-13 11:38:32 +0100

    slider of 'haylur'

--- js/main.js
+++ js/main.js
@@ -46,4 +46,17 @@ $(function() {
 		}
 	})
 
+
+	$('#haylur_slider').carouFredSel({
+		auto: false,
+		prev: '#prev2',
+		next: '#next2',
+		pagination: "#pager2",
+		mousewheel: true,
+		swipe: {
+			onMouse: true,
+			onTouch: true
+		}
+	});
+
 })
